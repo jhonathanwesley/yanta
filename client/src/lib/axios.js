d7faaa813b120d8e1df3452904faa28ab35e0bda
jhonathanwesley <jhonwfdalves@gmail.com>
2025-06-17 14:21:38 -0300

    DEPLOYMENT READY
    feat: refatorar gerenciamento de notas com novas funcionalidades e componentes

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+// Em produção, não existe localhost então a URL base precisa ser dinâmica
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL,
 });
+
 export default api;
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,13 +11,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+};
 app.use(express.json()); // this middleware will parse the JSON bodies: res.body
 app.use(rateLimiter);
 
@@ -26,10 +30,15 @@ app.use(rateLimiter);
 //     next();
 // });
 
-// middleware
-app.use(express.json());
-
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });   
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
